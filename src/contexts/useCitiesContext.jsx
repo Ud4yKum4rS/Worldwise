166c89c0f7bb44e6bf8e001a3cdf37a23d6fdc44
Udaya Kumar S <udaydavis144@gmail.com>
2024-06-27 11:57:40 +0530

    fixed error

--- src/contexts/useCitiesContext.jsx
+++ src/contexts/useCitiesContext.jsx
@@ -1,11 +1,11 @@
-import { useContext } from "react";
-import { CitiesContext } from "./CitiesContext";
+// import { useContext } from "react";
+// import { CitiesContext } from "./CitiesContext";
 
-export function useCitiesContext() {
-  const context = useContext(CitiesContext);
-  if (context === undefined)
-    throw new Error(
-      "Trying to access context value outside the cities provider"
-    );
-  return context;
-}
+// export function useCitiesContext() {
+//   const context = useContext(CitiesContext);
+//   if (context === undefined)
+//     throw new Error(
+//       "Trying to access context value outside the cities provider"
+//     );
+//   return context;
+// }
